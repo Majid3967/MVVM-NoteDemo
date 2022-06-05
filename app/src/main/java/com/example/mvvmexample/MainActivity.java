6a0e113c9e514ee851a99b5f8a2429ffc8d3e7b3
Majid <majid.ali3967@gmail.com>
2022-06-05 15:40:31 +0500

    Initial

---
+++ app/src/main/java/com/example/mvvmexample/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.mvvmexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                //update RecyclerView
+                Log.i("OnChange","Working");
+            }
+        });
+    }
+}
